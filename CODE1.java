3ce785a9988b9a70724a6b3890dded92645abf75
Likhithlokesh <lokeshlikhith24@gmail.com>
2026-01-29 14:17:48 +0530

    Add files via upload

---
+++ CODE1.java
@@ -0,0 +1,14 @@
+class code1{
+public static void main(String[]args){
+    int i=10;
+    int j=20;
+    int sum=i+j;
+    int sub=i-j;
+    int mul=i*j;
+    int div=j/i;
+    System.out.println("sum="+sum);
+    System.out.println("sub="+sub);
+    System.out.println("mul="+mul);
+    System.out.println("div="+div);
+}
+}
---
+++ code4.java
@@ -0,0 +1,8 @@
+class code4{
+    public static void main(String[]args){
+        char name='m';
+        int asciivalue =(int)name;
+        System.out.println("name:"+name);
+        System.out.println("asciivalue:"+asciivalue);
+    }
+}
